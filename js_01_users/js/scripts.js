51f688e9feb024a1afa3c9a3585e28bd7344ae4b
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-07-30 12:02:20 +0300

    user check done

--- js_01_users/js/scripts.js
+++ js_01_users/js/scripts.js
@@ -2,70 +2,46 @@
 
 var urersNumber = 5;
 
-var userList = [urersNumber]; // declare empty array
+var userList = []; // declare empty array
 
-var counter = 0;
+var pattern = new RegExp(/^[a-zA-Z\s]*$/); //only letters and white spaces allowed
 
-while (counter < urersNumber)
+var counter = 1;
+
+var resultMessage;
+
+// get the list of users and add to our users list array
+while (counter <= urersNumber)
 {
-    var userName = prompt("Please, give the name of user number " + (counter + 1), '');
-    var pattern = new RegExp("^[a-zA-Z\s]*$");
-    if (pattern.test(userName))
+    var userName = prompt("Please, give the name of user number " + counter, '');
+    if (pattern.test(userName) && userName != '' && userName != null)
     {
         userList.push(userName);
         counter++;
-        console.log("OK!");
     }
 }
 
-console.log(userList);
-
-var a, b;
-
-// ask the user to input data till it's an integer
-do {
-    a = prompt("Give me a base number", '');
-} while ((a % 1) || isNaN(a));
+// get admin name from the user
+var adminName;
 
 do {
-    b = prompt("Give me an exponent number", '');
-} while ((b % 1) || isNaN(b));
-    
+    adminName = prompt("What is your name?", '');
+} while (!(pattern.test(adminName)) || adminName == '' || adminName == null);
 
-var result;
-
-if (a != null && b != null)
-{
-    result = myPow(a, b);
-}
-else
-{
-    result = 'base number or exponent number or both of them were not provided!';
-}
-
-console.log(result);
-
-function myPow(a, b) {
-
-    var result = 1;
-
-    if (b >= 0) {
-        for (var i = 0; i < b; i++) {
-            result = result * a;
-        }
-
-        return result;
+// check if admin name is in the list of users
+for (var i = 0; i < userList.length; i++) {
+    if (userList[i] == adminName) {
+        alert(adminName + ', you succesfully logged to the system!');
+        resultMessage = adminName + ', you succesfully logged to the system!';
+        i = userList.length; // this way we stop the check
+    } else if (i == userList.length - 1) {
+        alert('Error: user not found!');
+        resultMessage = 'Error: user not found!';
     }
-    else {
-        b = b * (-1);
-        result = 1 / myPow(a, b);
-        return result;
-    }
-
 }
 
 showResult();
 
 function showResult() {
-    document.getElementById("powerResult").innerHTML = 'Our result is: ' + a + '<sup>' + b + '</sup>' + ' = ' + result;
+    document.getElementById("userResult").innerHTML = resultMessage;
 }
