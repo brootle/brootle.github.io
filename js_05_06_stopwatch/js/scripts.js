983bbd30f6998d938b07c3e81c79d0a2dc7014e9
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-06 22:33:10 +0300

    closed the js script

--- js_05_06_stopwatch/js/scripts.js
+++ js_05_06_stopwatch/js/scripts.js
@@ -1,91 +1,69 @@
-var initialTimestamp = Date.now();
+(function() {
 
-var timeCounted = 0;
+    var initialTimestamp = Date.now();
 
-var currentTimestamp;
+    var timeCounted = 0;
 
-var timecounter = document.querySelector('.timecounter');
+    var currentTimestamp;
 
-var milliseconds = document.querySelector('.milliseconds');
-var seconds = document.querySelector('.seconds');
-var minutes = document.querySelector('.minutes');
-var hours = document.querySelector('.hours');
+    var timecounter = document.querySelector('.timecounter');
 
-function updateStopwatch() {
+    var milliseconds = document.querySelector('.milliseconds');
+    var seconds = document.querySelector('.seconds');
+    var minutes = document.querySelector('.minutes');
+    var hours = document.querySelector('.hours');
 
-    currentTimestamp = Date.now() + timeCounted;
+    function updateStopwatch() {
 
-    timecounter.innerHTML = currentTimestamp - initialTimestamp;
+        currentTimestamp = Date.now() + timeCounted;
 
-    milliseconds.innerHTML = (currentTimestamp - initialTimestamp) % 1000;
+        timecounter.innerHTML = currentTimestamp - initialTimestamp;
 
-    var stopwatchSeconds = Math.floor((currentTimestamp - initialTimestamp) / 1000);
-    if (stopwatchSeconds > 59) {
-        stopwatchSeconds = stopwatchSeconds % 60;
+        milliseconds.innerHTML = (currentTimestamp - initialTimestamp) % 1000;
+
+        var stopwatchSeconds = Math.floor((currentTimestamp - initialTimestamp) / 1000);
+        if (stopwatchSeconds > 59) {
+            stopwatchSeconds = stopwatchSeconds % 60;
+        }
+        seconds.innerHTML = stopwatchSeconds;
+
+        var stopwatchMinutes = Math.floor((currentTimestamp - initialTimestamp) / 1000 / 60);
+        if (stopwatchMinutes > 59) {
+            stopwatchMinutes = stopwatchMinutes % 60;
+        }
+        minutes.innerHTML = stopwatchMinutes;
+
+        var stopwatchHours = Math.floor((currentTimestamp - initialTimestamp) / 1000 / 60 / 60);
+        if (stopwatchHours > 59) {
+            stopwatchHours = stopwatchHours % 60;
+        }
+        hours.innerHTML = stopwatchHours;
     }
-    seconds.innerHTML = stopwatchSeconds;
 
-    var stopwatchMinutes = Math.floor((currentTimestamp - initialTimestamp) / 1000 / 60);
-    if (stopwatchMinutes > 59) {
-        stopwatchMinutes = stopwatchMinutes % 60;
+    var startButton = document.querySelector('.buttons__start');
+    var stopButton; // this button doesn't exist untill we press start button
+
+    var intervalPointer;
+
+    function startStopwatch() {
+        initialTimestamp = Date.now(); // get current timestamp
+        intervalPointer = setInterval(updateStopwatch, 1); // start updating time
+
+        // change srat button to stop button
+        startButton.classList.remove('buttons__start');
+        startButton.classList.add('buttons__stop');
+        startButton.innerHTML = 'Stop';
+        stopButton = document.querySelector('.buttons__stop');
+
+        startButton.removeEventListener('click', startStopwatch);
+        stopButton.addEventListener('click', stopStopwatch);
     }
-    minutes.innerHTML = stopwatchMinutes;
 
-    var stopwatchHours = Math.floor((currentTimestamp - initialTimestamp) / 1000 / 60 / 60);
-    if (stopwatchHours > 59) {
-        stopwatchHours = stopwatchHours % 60;
-    }
-    hours.innerHTML = stopwatchHours;
-}
+    function stopStopwatch() {
+        clearInterval(intervalPointer); // stops repeating function
 
-var startButton = document.querySelector('.buttons__start');
-var stopButton; // this button doesn't exist untill we press start button
-
-var intervalPointer;
-
-function startStopwatch() {
-    initialTimestamp = Date.now(); // get current timestamp
-    intervalPointer = setInterval(updateStopwatch, 1); // start updating time
-
-    // change srat button to stop button
-    startButton.classList.remove('buttons__start');
-    startButton.classList.add('buttons__stop');
-    startButton.innerHTML = 'Stop';
-    stopButton = document.querySelector('.buttons__stop');
-
-    startButton.removeEventListener('click', startStopwatch);
-    stopButton.addEventListener('click', stopStopwatch);
-}
-
-function stopStopwatch() {
-    clearInterval(intervalPointer); // stops repeating function
-
-    // we must save current time that was already counted
-    timeCounted = currentTimestamp - initialTimestamp;
-
-    stopButton.classList.remove('buttons__stop');
-    stopButton.classList.add('buttons__start');
-    stopButton.innerHTML = 'Start';
-    startButton = document.querySelector('.buttons__start');
-
-    stopButton.removeEventListener('click', stopStopwatch);
-    startButton.addEventListener('click', startStopwatch);
-}
-
-startButton.addEventListener('click', startStopwatch);
-
-var resetButton = document.querySelector('.button__reset');
-
-function resetStopwatch() {
-    // here we must stop the function
-    clearInterval(intervalPointer); // stops repeating function
-    // zero all numbers
-    timeCounted = 0;
-    initialTimestamp = Date.now();
-    updateStopwatch();
-
-    // if button is STOP we must change it to START
-    if (startButton.innerHTML === 'Stop') {
+        // we must save current time that was already counted
+        timeCounted = currentTimestamp - initialTimestamp;
 
         stopButton.classList.remove('buttons__stop');
         stopButton.classList.add('buttons__start');
@@ -95,6 +73,32 @@ function resetStopwatch() {
         stopButton.removeEventListener('click', stopStopwatch);
         startButton.addEventListener('click', startStopwatch);
     }
-}
 
-resetButton.addEventListener('click', resetStopwatch);
+    startButton.addEventListener('click', startStopwatch);
+
+    var resetButton = document.querySelector('.button__reset');
+
+    function resetStopwatch() {
+        // here we must stop the function
+        clearInterval(intervalPointer); // stops repeating function
+        // zero all numbers
+        timeCounted = 0;
+        initialTimestamp = Date.now();
+        updateStopwatch();
+
+        // if button is STOP we must change it to START
+        if (startButton.innerHTML === 'Stop') {
+
+            stopButton.classList.remove('buttons__stop');
+            stopButton.classList.add('buttons__start');
+            stopButton.innerHTML = 'Start';
+            startButton = document.querySelector('.buttons__start');
+
+            stopButton.removeEventListener('click', stopStopwatch);
+            startButton.addEventListener('click', startStopwatch);
+        }
+    }
+
+    resetButton.addEventListener('click', resetStopwatch);
+
+})();
