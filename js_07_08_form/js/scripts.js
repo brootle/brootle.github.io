c3bd2b68411a3b1c8b4e252c915c59e02f0e7a67
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-13 13:26:10 +0300

    button added

--- js_07_08_form/js/scripts.js
+++ js_07_08_form/js/scripts.js
@@ -6,29 +6,39 @@ $(function () {
     // get all inputs in a form
     var inputsList = $('fieldset input');
 
-    console.log(inputsList);
-
     var tooltip;
 
     $(inputsList).hover(
-    function () {
+        function () {
+
+            // we call remove in case some tooltip was added by other function
+            $('.tooltip').remove();
         
-        tooltip = $(this).attr('title');
-        console.log(tooltip);
+            tooltip = $(this).attr('title');
 
-        // remove tittle attribute
-        $(this).removeAttr('title');
+            // remove tittle attribute
+            $(this).removeAttr('title');
 
-        // now let's show tooltip on the right from the input
-        $('<a class=' + 'tooltip'+ '>' + tooltip + '</a>').insertAfter(this);
+            // now let's show tooltip on the right from the input
+            $('<a class=' + 'tooltip'+ '>' + tooltip + '</a>').insertAfter(this);
         
-    },
-    function () {
-        // restore tittle attribute
-        $(this).attr('title', tooltip);
+        },
+        function () {
+            // restore tittle attribute
+            $(this).attr('title', tooltip);
 
-        // remove tooltip
-        $('.tooltip').remove();
-    }
-);
+            // remove tooltip
+            $('.tooltip').remove();
+    });
+
+    $('.help-button').on('click', function () {
+        console.log('clicked');
+        // make a loop
+        // in the loop we add tooltip
+        inputsList.each(function () {
+            tooltip = $(this).attr('title');
+            $('<a class=' + 'tooltip' + '>' + tooltip + '</a>').insertAfter(this);
+            //console.log($(this).attr('title'));
+        });
+    });
 });
