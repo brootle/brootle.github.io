70067f0757cd3b95cadc708c07785d306826f69a
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-15 10:50:15 +0300

    two plugins together

---
+++ js_09_10_jCarousel/js/scripts.js
@@ -0,0 +1,10 @@
+$(function () {
+    console.log('DOM loaded with jQuery - short version');
+
+    // we initiate the plugin
+    $(".fancybox").fancybox();
+
+    $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+});
