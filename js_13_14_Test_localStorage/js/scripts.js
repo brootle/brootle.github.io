a57d7590d8fec87e9980710e3e789639b040f20e
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-24 21:21:02 +0300

    set true in JSON to 1 and false to 0

--- js_13_14_Test_localStorage/js/scripts.js
+++ js_13_14_Test_localStorage/js/scripts.js
@@ -1,6 +1,7 @@
 $(function () {
     console.log('DOM loaded with jQuery - short version');
 
+    // we get test questions with reply options and save to localStorage, but correct replies are not defined
     $.getJSON("https://brootle.github.io/js_13_14_Test_localStorage/test.json", function (data) {
         // get JSON data from a file as an object and save it to local storage as JSON
         localStorage.setItem('examQuestions', JSON.stringify(data));
@@ -10,8 +11,7 @@ $(function () {
     var softwareTest = localStorage.getItem('examQuestions'); // get JSON formatted string from local storage
 
     softwareTest = JSON.parse(softwareTest); // convert JSON format string to JavaScript object
-
-    console.log(softwareTest);
+    
 
     var html = $('#exam').html();
 
@@ -39,36 +39,102 @@ $(function () {
 
     // add event listener on submit button
 
-    $('.submit-button').on('click', collectResults);
+    $('.submit-button').on('click', creatResultsObject);
 
-    var testResults = new Object;
-    console.log(testResults);
-
-    function collectResults(e) {
+    function creatResultsObject(e) {
+        var testResults = new Object;
         e.preventDefault();
         console.log('check results');
 
         for (var i = 0; i < $('.question').length; i++) {
             // here we get questions
-            console.log($('.question').eq(i).children(".question__tittle").html());
+
+            var testResultsKey = $('.question').eq(i).children(".question__tittle").html();
+            //console.log(key);
+
+            var valueObject = new Object;
 
             // now we get replies 
             for (var j = 0; j < $('.question').eq(i).children(".question__reply").length; j++) {
-                // get reply which is inside label
-                console.log($('.question').eq(i).children(".question__reply").eq(j).children("label").html());
 
-                console.log(' : ');
+                // get reply which is inside label
+                var key = $('.question').eq(i).children(".question__reply").eq(j).children("label").html();
+                //console.log(key);
 
                 // see if user checked reply or not
-                console.log($('.question').eq(i).children(".question__reply").eq(j).children("input").prop('checked'));
+                var value = $('.question').eq(i).children(".question__reply").eq(j).children("input").prop('checked');
+                //console.log(value);
+
+                // add key and value to our Object that has asnwers
+                valueObject[key] = value;
             }
+
+            // add object with reply and its status to the object that keeps all results of the test
+
+            testResults[testResultsKey] = valueObject;
         }
+        //console.log(testResults);
+
+        // now we get questions and defined correct replies
+        $.getJSON("https://brootle.github.io/js_13_14_Test_localStorage/testWithReplies.json", callbackFuncWithData);
+
+        function callbackFuncWithData(data) {
+            localStorage.setItem('examQuestionsAndReplies', JSON.stringify(data));
+        }
+
+        var testWithResults = localStorage.getItem('examQuestionsAndReplies'); // get JSON formatted string from local storage
+
+        testWithResults = JSON.parse(testWithResults); // convert JSON format string to JavaScript object
+
+        //console.log(testWithResults);
+
+        // now we need to run a function that will compare results
+
+        var errors = compareTestResults(testResults, testWithResults);
+        console.log(errors);
+
+        // now we should show modal window with results to the user
+
+        // after we compare resuts - remove correct from memory
+        localStorage.removeItem('examQuestionsAndReplies');
     }
 
-    // we send use Object without correct answers!
-    // another JSON with questions, a answers and correct ones must be in separate JSON file not accessible by use
-    // we must set permissions to make it accessable by server only
+    function compareTestResults(userResults, validResults) {
+        var numberOfErrors = 0;
 
-    // we should run through the page and build an object
+        // this code we use in html document as a template
+        var keys = Object.keys(userResults);
+
+        var validKeys = Object.keys(validResults);
+
+        for (var i = 0; i < keys.length; i++) {
+            //console.log(keys[i]);
+
+            var replies = userResults[keys[i]];
+
+            var validReplies = validResults[validKeys[i]];
+
+            keys_replies = Object.keys(replies);
+
+            valid_keys_replies = Object.keys(validReplies);
+
+            for (var j = 0; j < keys_replies.length; j++) {
+                //console.log(keys_replies[j] + ':' + replies[keys_replies[j]]);
+                //console.log(valid_keys_replies[j] + ':' + validReplies[valid_keys_replies[j]]);
+
+                //console.log(replies[keys_replies[j]] + ' : ' + validReplies[valid_keys_replies[j]]);
+
+                if (replies[keys_replies[j]] == 'false') {
+                    numberOfErrors++;
+                }
+
+                console.log(validReplies[valid_keys_replies[j]]);
+            }
+        }
+
+
+        return numberOfErrors;
+    }
+
 
 });
