b9a0ed0f0bd31dc53bf12028744a658547347585
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-26 19:44:52 +0300

    google search

--- js_15_16_GoogleAPI/js/scripts.js
+++ js_15_16_GoogleAPI/js/scripts.js
@@ -2,6 +2,31 @@ $(function () {
 
     'use strict'
     
+    //Your search engine ID:
+    //004155958433879807615:oz0xev5kykc
+
+    //Key
+    //AIzaSyAHIsWIFjkev1zQdgRWXSA4-oD-aWmlSOg
+
+    //GET https://www.googleapis.com/customsearch/v1?key=AIzaSyAHIsWIFjkev1zQdgRWXSA4-oD-aWmlSOg&cx=004155958433879807615:oz0xev5kykc&q=goit
+
+
+    // callback function
+    function GoogleCallback(func, data) {
+        window[func](data);
+    }
+
+    // change key!!!
+    $.getJSON("https://www.googleapis.com/customsearch/v1?key=AIzaSyAHIsWIFjkev1zQdgRWXSA4-oD-aWmlSOg&cx=004155958433879807615:oz0xev5kykc&q=goit&callback=GoogleCallback&context=?",
+    function (data) {
+        var ul = document.createElement("ul");
+        $.each(data.results, function (i, val) {
+            var li = document.createElement("li");
+            li.innerHTML = '<a href="' + val.url + '" title="' + val.url + '" target="_blank">' + val.title + "</a> - " + val.content;
+            ul.appendChild(li);
+        });
+        $('body').html(ul);
+    });
 
 
 });
