a055aad1f4d3eb0b9aa63e9399bcbd909c1568dc
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-26 19:26:00 +0300

    google search

--- js_15_16_GoogleAPI/js/scripts.js
+++ js_15_16_GoogleAPI/js/scripts.js
@@ -3,10 +3,5 @@ $(function () {
     'use strict'
     
 
-    url="http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=AAA&callback=?";
-
-    $.getJSON(url, function(data) {
-        alert('hello 1');
-    });
 
 });
