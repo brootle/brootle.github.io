ba70de0a9dc52b1297a93ae6f9502248a24e6a20
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-08-27 11:20:22 +0300

    using search API

---
+++ js_15_16_SearchAPI/js/scripts.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    'use strict'
+
+    //$.ajax({
+    //    // AJAX-specified URL
+    //    // callback function with parameters must be in url
+    //    url: "http://ajax.googleapis.com/ajax/services/search/web",
+    //    data: {
+    //        abc: 123
+    //    },
+    //    method: 'POST',
+    //    dataType: "jsonp",
+    //    // Handle the success event
+    //    success: function (data) {
+    //        // equal to previuos example
+    //        // ...
+
+    //        console.log('connected to server');
+    //    },
+    //    error: function () {
+
+    //    }
+    //});
+
+    //function GoogleCallback(jQueryObject, data) {
+    //    console.log(data);
+    //}
+
+});
