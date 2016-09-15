23b85e35c40af3e6511829b1a983585d3145035a
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-09-15 10:45:52 +0300

    all scripts added

--- js_19_20_scss/src/scripts/scripts.js
+++ js_19_20_scss/src/scripts/scripts.js
@@ -30,4 +30,31 @@ $(function () {
         }
     );
 
+    $('.panel').on('click', function (event) {
+
+        // we should not do anything if text clicked, only if panel clicked
+        if (!$(event.target).hasClass('panel-text') && event.target.tagName != "P") {
+
+            // check which panel was clicked, white or orange
+            var whitePanel = $(this).children('.panel-navigation--default');
+
+            if (whitePanel.length) {
+                $(this).find('.panel-navigation__plus--default').css("border-right", "2px solid #f5c63c").html('&#x2012;');
+                $(this).children('.panel-navigation--default').removeClass('panel-navigation--default').addClass('panel-navigation');
+                $(this).children('.panel-text').css("display", "block");
+            } else {
+                $(this).find('.panel-navigation__plus--default').css("border-right", "2px solid #e8e8e8").html('+');
+                $(this).children('.panel-navigation').removeClass('panel-navigation').addClass('panel-navigation--default');
+                $(this).children('.panel-text').css("display", "none");
+            }
+        }
+
+    });
+
+
+
+    //$('.panel-text').css("display", "none");
+
+    //&#8210;
+
 });
