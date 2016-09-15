bc44e43a8f47993d38528cb66011f5662d44ce2c
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-09-15 13:29:40 +0300

    installed gulp

---
+++ js_19_20_scss/gulpfile.js
@@ -0,0 +1,11 @@
+//  https://markgoodyear.com/2014/01/getting-started-with-gulp/
+
+var gulp = require('gulp');
+
+gulp.task('test', function () {
+    console.log('testing gulp')
+});
+
+gulp.task('default', function () {
+    
+});
