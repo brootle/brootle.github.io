0bb4ca93519f01270e012d06818f441536754633
brootle <luganskukraine@gmail.com>
2016-10-10 16:57:23 +0300

    slider works

--- js_final_sliderapp/scripts/controller.js
+++ js_final_sliderapp/scripts/controller.js
@@ -5,8 +5,41 @@ define(
 
         function Controller(id, model, view) {
         
-            view.goRight.addEventListener("click",nextImage);   
-            view.goLeft.addEventListener("click",previousImage);           
+            view.goRight.addEventListener("click",glideNext);   
+            view.goLeft.addEventListener("click",glidePrevious);     
+
+            function glideNext() {
+                var elem = view.silderContainer.firstChild; 
+                var pos = view.silderContainer.offsetWidth * (-1);
+                var id = setInterval(frame, 5);
+                function frame() {
+                    if (pos == view.silderContainer.offsetWidth * (-2)) {
+                        clearInterval(id);
+                        nextImage();
+                        elem.style.left = view.silderContainer.offsetWidth * (-1) + 'px';
+                    } else {
+                        pos--; 
+                        elem.style.left = pos + 'px'; 
+                    }
+                }
+            }       
+
+            function glidePrevious() {
+                var elem = view.silderContainer.firstChild; 
+                var pos = view.silderContainer.offsetWidth * (-1);
+                var id = setInterval(frame, 5);
+                function frame() {
+                    if (pos == 0) {
+                        clearInterval(id);
+                        previousImage();
+                        elem.style.left = view.silderContainer.offsetWidth * (-1) + 'px';
+                    } else {
+                        pos++; 
+                        elem.style.left = pos + 'px'; 
+                    }
+                }
+            }                   
+             
 
             function nextImage(){
 
@@ -15,7 +48,7 @@ define(
                     model.centralImageIndex = 0;
                 }
 
-                view.render(id, model.images, model.centralImageIndex);                 
+                view.render(id, model.images, model.centralImageIndex);               
             }
 
             function previousImage(){
