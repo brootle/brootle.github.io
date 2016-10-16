d935cd53d0bd5db32b159982296cc5280153eb86
brootle <luganskukraine@gmail.com>
2016-10-16 12:25:35 +0300

    added vertical align of tex in masonry

--- js_final/dist/assets/scripts/main.js
+++ js_final/dist/assets/scripts/main.js
@@ -61,11 +61,15 @@ requirejs( [
             // here we also must add text
             items[i].childNodes[0].childNodes[0].innerHTML = data.hits[i].tags;
             // after text added top property must be calculated and set
+            //console.log(items[i].childNodes[0].childNodes[0].offsetHeight);
+            //console.log(items[i].offsetHeight);
+            items[i].childNodes[0].childNodes[0].style.top = (items[i].offsetHeight - items[i].childNodes[0].childNodes[0].offsetHeight) / 2 + "px";
         }
 
     }
 
     sendRequest("vacation+travel");
+
 
     ////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////
