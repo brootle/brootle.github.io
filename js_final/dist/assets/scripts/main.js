dc8742b364750b592c1f167a243312e2986ff5d3
Alexander Cherednichenko <luganskukraine@gmail.com>
2016-10-20 09:44:51 +0300

    added masonry block using requirejs config

--- js_final/dist/assets/scripts/main.js
+++ js_final/dist/assets/scripts/main.js
@@ -1,8 +1,127 @@
 
-requirejs( [
-  //'https://unpkg.com/masonry-layout@4.1.1/dist/masonry.pkgd.js',
-  'https://libraries.cdnhttps.com/ajax/libs/masonry/3.2.2/masonry.pkgd.js',
-], function(Masonry) {
+// requirejs( [
+//   //'https://unpkg.com/masonry-layout@4.1.1/dist/masonry.pkgd.js',
+//   'https://libraries.cdnhttps.com/ajax/libs/masonry/3.2.2/masonry.pkgd.js',
+// ], function(Masonry) {
+
+//     var elem = document.querySelector('.grid');
+//     var msnry = new Masonry( elem, {
+//         // options
+//         itemSelector: '.grid-item',
+//         columnWidth: '.grid-sizer',
+//         gutter: 10,
+//         percentPosition: true
+//     });
+
+//     // Add filter to all grid-item elements
+
+//     var items = document.getElementsByClassName("grid-item");
+//     //console.log(items);
+//     for(var i = 0; i < items.length; i++){
+//         items[i].innerHTML = '<div><a></a></div>';
+//     }
+
+//     ////////////////////////////////////////////////////////
+//     ////////////////////////////////////////////////////////
+
+//     function sendRequest(key){
+//         var request;
+//         var url = "https://pixabay.com/api/?key=3531240-ec0d55581e7ceac4acc8e28c0&image_type=photo&pretty=true&per_page=7&orientation=horizontal&q=" + key;
+        
+//         if (window.XMLHttpRequest) {
+//             request = new XMLHttpRequest();
+//         } else {
+//             // code for older browsers
+//             request = new ActiveXObject("Microsoft.XMLHTTP");
+//         }
+//         request.onreadystatechange = function(data) {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 data = JSON.parse(this.responseText);
+//                 updatePhotos(data);
+//             }
+//         };
+//         request.open("GET", url, true);
+//         request.send();
+//     }
+
+
+//     function updatePhotos(data){
+//         //console.log(data.hits[0].tags);
+//         //console.log(data.hits[0].webformatURL);
+//         var items = document.getElementsByClassName("grid-item");
+
+//         for(var i = 0; i < data.hits.length; i++){
+//             // add photos to masonry layout
+//             items[i].style.background = "url('"+ data.hits[i].webformatURL +"') no-repeat center center"; 
+//             items[i].style.backgroundSize = "cover";
+
+//             // here we also must add text
+//             items[i].childNodes[0].childNodes[0].innerHTML = data.hits[i].tags;
+//             // after text added top property must be calculated and set
+//             items[i].childNodes[0].childNodes[0].style.top = (items[i].offsetHeight - items[i].childNodes[0].childNodes[0].offsetHeight) / 2 + "px";
+//         }
+
+//     }
+    
+//     // when we load the page we upload random pics out 10 queries
+//     var randomKey = ["vacation", "travel", "sea", "beach", "mountains", "new york", "japan", "ukraine","england","france"];
+//     var index = Math.floor(Math.random() * 10);
+//     sendRequest(randomKey[index]);
+
+//     window.addEventListener("resize", function(){
+//         // updating text position on window size changed;
+//         var items = document.getElementsByClassName("grid-item");
+//         for(var i = 0; i < items.length; i++){
+//             items[i].childNodes[0].childNodes[0].style.top = (items[i].offsetHeight - items[i].childNodes[0].childNodes[0].offsetHeight) / 2 + "px";
+//         }
+//     });      
+
+//     ////////////////////////////////////////////////////////////
+//     ////////////////////////////////////////////////////////////
+
+//     // now we should add event listener on seach button click
+//     document.getElementById("search-block__button").addEventListener("click", function(){
+//         var key = document.getElementById("search-block__query").value;
+//         sendRequest(key);
+//         document.getElementById("search-block__query").value = "";
+//     });
+
+//     // and add listener on Enter in text field
+//     document.getElementById("search-block__query").addEventListener("keyup", function (e) {
+//         if (e.keyCode == 13) {
+//             var key = document.getElementById("search-block__query").value;
+//             sendRequest(key);
+//             document.getElementById("search-block__query").value = "";
+//         }
+//     });       
+  
+
+// });
+
+requirejs.config({
+    paths: {
+        'masonry' : 'https://libraries.cdnhttps.com/ajax/libs/masonry/3.2.2/masonry.pkgd'
+    },
+    shim:{
+        'masonry' : {
+            exports: 'Masonry'
+        }
+    }
+});
+
+
+require(
+    [   
+        'model',
+        'view',
+        'controller',
+        'masonry'
+    ],
+    function(Model,View,Controller,Masonry){
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+/// MASONRY BLOCK                                                                           ///
+///////////////////////////////////////////////////////////////////////////////////////////////
 
     var elem = document.querySelector('.grid');
     var msnry = new Masonry( elem, {
@@ -93,18 +212,14 @@ requirejs( [
             sendRequest(key);
             document.getElementById("search-block__query").value = "";
         }
-    });       
-  
+    });    
 
-});
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-require(
-    [   
-        'model',
-        'view',
-        'controller'
-    ],
-    function(Model,View,Controller){
+////////////////////////////////////////////////////////////////////////////////////////////////
+// this is a block for SLIDER                                                                 //
+////////////////////////////////////////////////////////////////////////////////////////////////
 
         var images = [
             'dist/assets/images/1.jpg',
