2dd2ab10a927684fb6870253c87a7e8d9dad29e9
brootle <luganskukraine@gmail.com>
2017-02-12 16:49:57 +0200

    update

--- freecodecamp_wikireader/js/main.js
+++ freecodecamp_wikireader/js/main.js
@@ -2,20 +2,31 @@ $(function () {
   
 // https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
 
-  var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json';
+  $.ajax({
+      type: "GET",
+      url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      success: function (data, textStatus, jqXHR) {
 
-  // send request to the weather server
-  $.ajax( {
-      url: url,
-      //data: queryData,
-      //dataType: 'json',
-      type: 'POST',
-      headers: { 'Api-User-Agent': 'Example/1.0' },
-      success: function(data) {
+          var markup = data.parse.text["*"];
+          var blurb = $('<div></div>').html(markup);
 
-        console.log(data);
+          // remove links as they will not work
+          blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+
+          // remove any references
+          blurb.find('sup').remove();
+
+          // remove cite error
+          blurb.find('.mw-ext-cite-error').remove();
+          $('#article').html($(blurb).find('p'));
+
+      },
+      error: function (errorMessage) {
       }
-
-  } );    
+  });
+   
 
 });
