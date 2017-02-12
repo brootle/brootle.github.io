b4c8370141fabffc1ffa1fffd8a51b4231cfe225
brootle <luganskukraine@gmail.com>
2017-02-12 16:38:30 +0200

    changes

--- freecodecamp_wikireader/js/main.js
+++ freecodecamp_wikireader/js/main.js
@@ -2,23 +2,20 @@ $(function () {
   
 // https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
 
-  var coordinates = '';
-  var url = 'https://en.wikipedia.org/w/api.php';
-  var queryData = "alex";
-  
+  var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json';
 
   // send request to the weather server
   $.ajax( {
       url: url,
-      data: queryData,
-      dataType: 'json',
+      //data: queryData,
+      //dataType: 'json',
       type: 'POST',
       headers: { 'Api-User-Agent': 'Example/1.0' },
       success: function(data) {
 
         console.log(data);
       }
-      
+
   } );    
 
 });
