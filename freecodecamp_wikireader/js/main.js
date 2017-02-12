93567b83484c1433833510bdefe846b647b8837a
brootle <luganskukraine@gmail.com>
2017-02-12 16:22:53 +0200

    wiki reader

---
+++ freecodecamp_wikireader/js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+  
+// https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
+
+  var coordinates = '';
+  var url = 'https://en.wikipedia.org/w/api.php';
+  var queryData = "alex";
+  
+
+  // send request to the weather server
+  $.ajax( {
+      url: url,
+      data: queryData,
+      dataType: 'json',
+      type: 'POST',
+      headers: { 'Api-User-Agent': 'Example/1.0' },
+      success: function(data) {
+
+        console.log(data);
+      }
+      
+  } );    
+
+});
