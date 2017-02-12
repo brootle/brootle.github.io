736fc9996f5bf91825494409e976d04f96fbf2ed
brootle <luganskukraine@gmail.com>
2017-02-12 22:25:32 +0200

    final wiki reader

--- freecodecamp_wikireader/js/main.js
+++ freecodecamp_wikireader/js/main.js
@@ -1,32 +1,66 @@
 $(function () {
   
-// https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
+  //var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?";
+  //var url = "https://en.wikipedia.org/w/api.php?action=query&titles=alex&format=json&callback=?";
 
-  $.ajax({
-      type: "GET",
-      url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
-      contentType: "application/json; charset=utf-8",
-      async: false,
-      dataType: "json",
-      success: function (data, textStatus, jqXHR) {
+// with images and text https://en.wikipedia.org/w/api.php?format=xml&action=query&generator=search&gsrnamespace=0&gsrsearch=alex&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max
 
-          var markup = data.parse.text["*"];
-          var blurb = $('<div></div>').html(markup);
+// with titles and snippets https://en.wikipedia.org/w/api.php?action=query&list=search&format=xml&srsearch=alex&srnamespace=0&srprop=snippet&srlimit=10
 
-          // remove links as they will not work
-          blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+// with full url https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=alex&format=xml&gsrprop=snippet&prop=info&inprop=url
 
-          // remove any references
-          blurb.find('sup').remove();
+// title + url + desctiption https://en.wikipedia.org/w/api.php?action=opensearch&search=alex&format=xml&limit=20
 
-          // remove cite error
-          blurb.find('.mw-ext-cite-error').remove();
-          $('#article').html($(blurb).find('p'));
 
-      },
-      error: function (errorMessage) {
+  $('.search-button').on('click', function () {
+          wikiSearch()
+  });
+  
+  $("#search-field").keyup(function (e) {
+      if (e.keyCode == 13) {
+          wikiSearch()
       }
   });
-   
+
+  function wikiSearch(){
+
+    var query = $("#search-field").val();
+    if (query === ''){
+      query = "null";
+    }
+
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + query + "&format=json&limit=20&callback=?";
+
+    $.getJSON({
+      type: "POST",
+      url: url,
+      dataType: "json"
+    })
+    .done(function(data, textStatus, jqXHR) {
+
+      console.log(data);
+
+      var html = '';
+
+      for(var i = 0; i < data[1].length; i++){
+        
+        html += "<a href='" + data[3][i] + "' target='_blank'>";
+
+        html +="<h2>" + data[1][i] + "</h2>";
+
+        html +="<p>" + data[2][i] + "</p>";
+
+        html += "</a>";
+
+      }
+
+      $(".search-result").html(html);
+
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      // log error to browser's console
+      console.log(errorThrown.toString());
+    });
+  }   
 
 });
