47fbc02c9c31d74440dd0f210c42cac01de060a2
brootle <luganskukraine@gmail.com>
2017-02-25 16:06:58 +0200

    simple calculator finished

--- freecodecamp_calculator/js/main.js
+++ freecodecamp_calculator/js/main.js
@@ -9,24 +9,19 @@ $(function () {
 
   // source = source.replace(/sin/ig,"Math.sin");
 
-  var x = 2;
-  var y = 3;
-  var z = 5;
-  var a = eval("2+x / y");
-
   //console.log(a);
 
   //console.log(eval("sin(2) + 3")); // Math.sin(3)
 
-  console.log(eval("Math.sin(2) + 3")); // Math.sin(3)
-  console.log(eval("Math.pow(2,3)")); // Math.sin(3)
-  // "x^4".replace(/(.*)\^(.*)/g, "Math.pow($1, $2)")
+  // console.log(eval("Math.sin(2) + 3")); // Math.sin(3)
+  // console.log(eval("Math.pow(2,3)")); // Math.sin(3)
+  // // "x^4".replace(/(.*)\^(.*)/g, "Math.pow($1, $2)")
 
-  console.log(eval("12 % 10")); //
+  // console.log(eval("12 % 10")); //
 
-  console.log(eval("1.111111111111111e+40 + 33333333333333333333333333333333333333333333333333333333333333333"));
+  // console.log(eval("1.111111111111111e+40 + 33333333333333333333333333333333333333333333333333333333333333333"));
 
-  console.log(eval("2 + 2")); //
+  //console.log(eval("2 + .4")); //
   //console.log(eval("2^2"));
 
   var resultValue = "0";
@@ -35,10 +30,60 @@ $(function () {
   $(".buttons-row div").on("click",function(){
     console.log($(this).text() + "-clicked");
 
-    // it's better to make CASE here to do some actions depending on what was clicked
+    if($("#result").text() === "Error"){
+      console.log($("#result").text());
+      resultValue = "0";
+      $("#result").text(resultValue);
+    }
+
+    if($(this).text() === '.'){
+      var lastSymbol = resultValue[resultValue.length-1];
+      //console.log(lastSymbol);
+      if(lastSymbol == '.'){
+        // do nothing
+      }else{
+        resultValue += '.';
+        $("#result").text(resultValue);
+        $("#reset").text("CE");
+      }      
+
+    }    
+
+    // remove last symbols
+    if($(this).text() === 'CE'){
+
+      if(resultValue.length === 1){
+        resultValue = "0";
+        $("#result").text(resultValue);
+      }else{
+
+        var lastSymbol = resultValue[resultValue.length-1];
+
+        if(lastSymbol == ' '){
+          resultValue = resultValue.substring(0, resultValue.length - 3);
+        }else{
+          resultValue = resultValue.substring(0, resultValue.length - 1);
+        }
+
+        // resultValue = "0";
+        $("#result").text(resultValue);
+        // $("#reset").text("CE");
+      }
+
+    }    
+
+    if($(this).text() === 'AC'){
+      resultValue = "0";
+      $("#result").text(resultValue);
+      $("#reset").text("CE");
+    }
 
     // if '=' clicked
     if($(this).text() === '='){
+
+      console.log(resultValue);
+
+      $("#reset").text("AC");
 
       console.log("EVALUATE!");
       // here we must also catch error
@@ -49,22 +94,42 @@ $(function () {
       resultValue = resultValue.replace(/×/ig,"*");
       resultValue = resultValue.replace(/÷/ig,"/");
 
-      var calculationsResult = eval(resultValue);
+      //var calculationsResult = eval(resultValue);
+      var calculationsResult = "Error";
+
+      try {
+          calculationsResult = eval(resultValue);
+      }
+      catch(err) {
+          
+      }      
+
+
       $("#result").text(calculationsResult);
     
 
-      resultValue = calculationsResult;
+      resultValue = calculationsResult.toString();
 
     } 
 
     // if '0-9' clicked
     // console.log(/[0-9]/.test($(this).text()));
     if(/[0-9]/.test($(this).text())){
-      if(resultValue == 0){
+
+      console.log(resultValue);
+
+      if($("#reset").text() ==  "AC"){
         resultValue = '';
       }
+
+      if(resultValue === "0"){
+        resultValue = '';
+      }
+
       resultValue += $(this).text();
       $("#result").text(resultValue);
+
+      $("#reset").text("CE");
 
     }
 
@@ -87,6 +152,8 @@ $(function () {
         }
         resultValue += ' ' + addOperator + ' ';
         $("#result").text(resultValue);
+
+        $("#reset").text("CE");
       }
 
       
