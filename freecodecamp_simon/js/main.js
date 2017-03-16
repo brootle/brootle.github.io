3e61700f93a5f1f3ef65b9f16d5e49d6d12ff9d1
brootle <luganskukraine@gmail.com>
2017-03-16 17:37:59 +0200

    removed buggy inner shadow

--- freecodecamp_simon/js/main.js
+++ freecodecamp_simon/js/main.js
@@ -2,7 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM loaded with JavaScript');   
 
+  // add event listenter to open and close
+  var buttons = document.querySelectorAll(".button");
+  buttons.forEach(button => button.addEventListener('click',playSound));
 
+  function playSound(e){
+    console.log(e.currentTarget);
+  }
 
 
 });
