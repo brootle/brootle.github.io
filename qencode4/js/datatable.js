112df115e11fd204d292fc64077f2f0714931339
brootle <luganskukraine@gmail.com>
2017-06-15 18:12:13 +0300

    integrated script for selecting number of rows

--- qencode4/js/datatable.js
+++ qencode4/js/datatable.js
@@ -20,7 +20,48 @@ $(document).ready(function(){
                 }
             },
             // see https://datatables.net/reference/option/language.info
-            info: "<span class='transparent-green'>_START_ - _END_</span> of _TOTAL_"
+            info: `
+                <div id='info-container'>
+
+                    <div id="rows_number_selector">
+                        <label class="input-label"><span>Rows:</span></label>
+                        <a><span>10</span></a>
+                        <a><span>20</span></a>
+                        <a><span>50</span></a>
+                        <a><span>100</span></a>
+                    </div>
+
+                    <span id='rows_number' class='transparent-green'>_START_ - _END_</span> of _TOTAL_
+                </div>
+                <script>
+                    // control table after it was initiates
+
+                    var table = $('#transfer_table').DataTable();
+
+                    // handle events when we click 'rows_number' to change number of rows displayed
+                    $('#rows_number').on('click', function(){
+                        $('#rows_number_selector').toggle();
+                    });
+
+                    // see here https://datatables.net/reference/api/page.len()
+                    $('#rows_number_selector a').on('click', function(){
+                        table.page.len( $(this).text() ).draw(); 
+                    });             
+
+                    // hide when clicked anywhere outside
+                    $(document).mouseup(function(e) 
+                    {
+                        var container = $('#rows_number_selector');
+
+                        // if the target of the click isn't the container nor a descendant of the container
+                        if (!container.is(e.target) && container.has(e.target).length === 0) 
+                        {
+                            container.hide();
+                        }
+                    });                    
+
+                </script>
+            `
         },
 
         "iDisplayLength": 5,
@@ -30,6 +71,7 @@ $(document).ready(function(){
         null,
         null,
         { "bSortable": false }
-        ] } );
+        ] } 
+    );
 
 });
