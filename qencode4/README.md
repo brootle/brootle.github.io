8c6ab618e29f40f5cf456d23372cf16d9b34cdfb
brootle <luganskukraine@gmail.com>
2017-06-20 14:18:59 +0300

    making billing cost managing page

--- qencode4/README.md
+++ qencode4/README.md
@@ -7,3 +7,10 @@ https://datatables.net/examples/plug-ins/range_filtering.html
 Datepicker
 https://jqueryui.com/datepicker/
 http://api.jqueryui.com/datepicker/
+
+
+For cool graph
+http://www.chartjs.org/samples/latest/charts/line/basic.html
+https://stackoverflow.com/questions/34403510/chart-js-straight-lines-instead-of-curves
+https://stackoverflow.com/questions/38222345/chartjs-line-charts-remove-color-underneath-lines
+http://jsfiddle.net/4vobe59a/
---
+++ qencode4/js/billing.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Billing loaded with JavaScript');
+    
+    const tabLinks = document.querySelectorAll('.tablinks');
+
+    tabLinks.forEach(link => link.addEventListener('click', switchTab));
+
+    function switchTab(e){
+
+        // find active elements and remove -> tablinks--active tabcontent--active
+        const activeLink = document.querySelector('.tablinks--active');
+        const activeContent = document.querySelector('.tabcontent--active');
+
+        activeLink.classList.remove('tablinks--active');
+        activeContent.classList.remove('tabcontent--active');
+
+        // find what tab link was clicked and show tab content with that ID
+        const clickedLink = e.currentTarget;
+        clickedLink.classList.add('tablinks--active');
+
+        const tabID = clickedLink.getAttribute("data-tabcontent");
+
+        const tabContent = document.querySelector(`#${tabID}`);
+        tabContent.classList.add('tabcontent--active');
+    }
+
+});
+
