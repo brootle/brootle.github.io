471a678bec9ba0ee55c5993cf8b965daaf1a8d14
brootle <luganskukraine@gmail.com>
2017-06-21 19:04:53 +0300

    tooltip added

--- qencode4/js/canvas_chart.js
+++ qencode4/js/canvas_chart.js
@@ -5,19 +5,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
 CanvasJS.addColorSet('greenShades',
      [//colorSet Array
-     'green',
-     'blue'
+     '#8fdb80', //green
+     '#98c4ed'  //blue
     ]);     
 
     var chart = new CanvasJS.Chart("chartContainer",
     {
+
+        toolTip: {
+			    content: "{x} <br> <strong>{label}</strong> min <br>  <strong>{name}: $ {y}</strong>",
+          cornerRadius:0,
+          fontFamily: "'Lato', sans-serif, sans-serif",
+          fontStyle:"normal",
+          fontColor: "#73767d",
+          borderThickness:0,
+          shared: false  //disable here. 
+        },      
 
         colorSet: "greenShades",
     //   title: {
     //     text: "Monthly Downloads",
     //     fontSize: 25
     //   },
+
+      axisY:{
+        maximum: 125,
+        tickThickness:1,
+        gridThickness: 1,
+        lineThickness:0,
+        interval:25,
+      //  title: "axisY Title",
+      //  interlacedColor: "#F8F1E4",
+        tickLength: 0
+      },
+
       axisX:{
+        lineThickness:0,
         valueFormatString: "MMM" ,
         interval: 1,
         intervalType: "month"
@@ -28,39 +51,69 @@ CanvasJS.addColorSet('greenShades',
     //   },
 
       data: [
-      {
+      { 
+        name: "SD",
         indexLabelFontColor: "green",
         type: "area",
+        fillOpacity: .3,
+        lineThickness: 3,
+        lineColor: "#49c332",
+        markerBorderColor:"#49c332",
+        markerBorderThickness:4,
+        markerColor: "white",
+        markerType: "none",
+        markerSize:6,
+        mouseover: onMouseover,
         dataPoints: [//array
 
-        { x: new Date(2012, 00, 1), y: 26},
-        { x: new Date(2012, 01, 1), y: 38 },
-        { x: new Date(2012, 02, 1), y: 43 },
-        { x: new Date(2012, 03, 1), y: 29 },
-        { x: new Date(2012, 04, 1), y: 41 },
-        { x: new Date(2012, 05, 1), y: 45 },
-        { x: new Date(2012, 06, 1), y: 86},
-        { x: new Date(2012, 07, 1), y: 64 },
-        { x: new Date(2012, 08, 1), y: 53 },
-        { x: new Date(2012, 09, 1), y: 60}
+        { x: new Date(2016, 00, 1), y: 22.04, label: "10023" },
+        { x: new Date(2016, 00, 26), y: 40.34, label: "10023" },
+        { x: new Date(2016, 01, 15), y: 65.00, label: "10023" },
+        { x: new Date(2016, 02, 3), y: 58.78, label: "10023" },
+        { x: new Date(2016, 03, 17), y: 40.03, label: "10023" },
+        { x: new Date(2016, 04, 20), y: 35.12, label: "10023" },
+        { x: new Date(2016, 05, 12), y: 45.97, label: "10023"},
+        { x: new Date(2016, 06, 18), y: 57.22, label: "10023" },
+        { x: new Date(2016, 07, 5), y: 68.55, label: "10023" },
+        { x: new Date(2016, 07, 26), y: 60.72, label: "10023" },
+        { x: new Date(2016, 08, 13), y: 63.22, label: "10023"},
+        { x: new Date(2016, 09, 1), y: 61.12, label: "10023"},
+        { x: new Date(2016, 09, 14), y: 53.86, label: "10023"},
+        { x: new Date(2016, 09, 28), y: 48.12, label: "10023"},
+        { x: new Date(2016, 11, 1), y: 35.34, label: "10023"}
         ]
       },
 
       {
+        name: "UHD",
         indexLabelFontColor: "blue",
         type: "area",
+        lineColor: "#1a74d4",
+        markerBorderColor:"#1a74d4",
+        markerBorderThickness:4,
+        markerColor: "white",
+        lineThickness: 3,
+        markerType: "none",
+        markerSize:6,
+        mouseover: onMouseover,
         dataPoints: [//array
 
-        { x: new Date(2012, 00, 1), y: 16},
-        { x: new Date(2012, 02, 13), y: 28 },
-        { x: new Date(2012, 02, 18), y: 33 },
-        { x: new Date(2012, 02, 30), y: 19 },
-        { x: new Date(2012, 04, 1), y: 21 },
-        // { x: new Date(2012, 05, 1), y: 5500 },
-        { x: new Date(2012, 07, 1), y: 96},
-        { x: new Date(2012, 07, 15), y: 24 },
-        { x: new Date(2012, 08, 4), y: 13 },
-        { x: new Date(2012, 09, 1), y: 70}
+        { x: new Date(2016, 00, 1), y: 14.33, label: "34023"},
+        { x: new Date(2016, 00, 22), y: 20.21, label: "10023" },
+        { x: new Date(2016, 01, 5), y: 50.11, label: "3023" },
+        { x: new Date(2016, 02, 1), y: 40.75, label: "10023" },
+        { x: new Date(2016, 02, 13), y: 43.34, label: "10023" },
+        { x: new Date(2016, 03, 3), y: 51.44, label: "10023" },
+        { x: new Date(2016, 05, 12), y: 20.90, label: "10023"},
+        { x: new Date(2016, 06, 18), y: 30.22, label: "10023" },
+        { x: new Date(2016, 06, 25), y: 38.45, label: "10023" },
+        { x: new Date(2016, 07, 12), y: 59.65, label: "10023"},
+        { x: new Date(2016, 08, 3), y: 64.21, label: "10023"},
+        { x: new Date(2016, 08, 24), y: 57.64, label: "10023"},
+        { x: new Date(2016, 09, 4), y: 47.43, label: "10023"},
+        { x: new Date(2016, 09, 19), y: 36.22, label: "10023"},
+        { x: new Date(2016, 10, 5), y: 34.78, label: "32023"},
+        { x: new Date(2016, 11, 1), y: 44.77, label: "34023"}
         ]
       }
 
@@ -137,6 +190,10 @@ CanvasJS.addColorSet('greenShades',
     // });
 
     chart.render();
+
+    function onMouseover(e){
+        e.dataPoint.markerType = "circle";
+    }    
 
 });
 
