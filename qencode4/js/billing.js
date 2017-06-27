8090a2e6ad5161cc112c7dbef9542d2f09f36d30
brootle <luganskukraine@gmail.com>
2017-06-27 13:34:41 +0300

    moved script into modaljs

--- qencode4/js/billing.js
+++ qencode4/js/billing.js
@@ -1,41 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Billing loaded with JavaScript');
 
-    //////////////////////////////////////////////////////////////////////////
-    // this is to handle the click on "See Payment History" in modal window //
-
-    const seePaymentHistory = document.querySelector("#see_payment_history");
-    seePaymentHistory.addEventListener('click', showPaymentHistoryTab);
-
-    function showPaymentHistoryTab(){
-        // find active elements and remove -> tablinks--active tabcontent--active
-        const activeLink = document.querySelector('.tablinks--active');
-        const activeContent = document.querySelector('.tabcontent--active');
-
-        activeLink.classList.remove('tablinks--active');
-        activeContent.classList.remove('tabcontent--active');
-
-        // find what tab link was clicked and show tab content with that ID
-        const clickedLink = document.querySelector('[data-tabcontent="history"]');
-        clickedLink.classList.add('tablinks--active');
-
-        const tabID = clickedLink.getAttribute("data-tabcontent");
-
-        const tabContent = document.querySelector(`#${tabID}`);
-        tabContent.classList.add('tabcontent--active');   
-
-        // after we showed the tab we scroll to it
-        document.getElementById("history").scrollIntoView();
-    }
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // function to scroll element into view
-    function scrollIntoView(eleID) {
-        var e = document.getElementById(eleID);
-        if (!!e && e.scrollIntoView) {
-            e.scrollIntoView();
-        }
-    }    
 
 
     /////////////////////////////////////////////////////////////////
--- qencode4/js/modal.js
+++ qencode4/js/modal.js
@@ -22,4 +22,40 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }    
 
+
+    //////////////////////////////////////////////////////////////////////////
+    // this is to handle the click on "See Payment History" in modal window //
+    
+    seePaymentHistory.addEventListener('click', showPaymentHistoryTab);
+
+    function showPaymentHistoryTab(){
+        // find active elements and remove -> tablinks--active tabcontent--active
+        const activeLink = document.querySelector('.tablinks--active');
+        const activeContent = document.querySelector('.tabcontent--active');
+
+        activeLink.classList.remove('tablinks--active');
+        activeContent.classList.remove('tabcontent--active');
+
+        // find what tab link was clicked and show tab content with that ID
+        const clickedLink = document.querySelector('[data-tabcontent="history"]');
+        clickedLink.classList.add('tablinks--active');
+
+        const tabID = clickedLink.getAttribute("data-tabcontent");
+
+        const tabContent = document.querySelector(`#${tabID}`);
+        tabContent.classList.add('tabcontent--active');   
+
+        // after we showed the tab we scroll to it
+        document.getElementById("history").scrollIntoView();
+    }
+    //////////////////////////////////////////////////////////////////////////////////
+
+    // function to scroll element into view
+    function scrollIntoView(eleID) {
+        var e = document.getElementById(eleID);
+        if (!!e && e.scrollIntoView) {
+            e.scrollIntoView();
+        }
+    }        
+
 });
