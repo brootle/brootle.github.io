e386b78a407c9b0182dff3b1b512138bd7fc1abc
brootle <luganskukraine@gmail.com>
2017-07-07 21:41:18 +0300

    sticky footer didn't work on mobile phone because they dont have vertical sroll bars

--- qencode4/js/sticky_footer.js
+++ qencode4/js/sticky_footer.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Sticky footer loaded with JavaScript');
 
     var footer = document.querySelector("footer");
+    var body = document.querySelector("body");
+    
     toogleStickyFooter();
 
     window.addEventListener("resize", toogleStickyFooter);
 
     function toogleStickyFooter(){
-        var hasVScroll = window.innerWidth > document.documentElement.clientWidth;
+
+        var hasVScroll = body.scrollHeight > (screen.height - footer.scrollHeight);
+
+
+        //var hasVScroll = window.innerWidth > document.documentElement.clientWidth;
 
         if(hasVScroll){
             // remove Sticky footer
