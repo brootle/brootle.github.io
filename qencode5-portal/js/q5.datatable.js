4837e9ecab11bf2ab72ac428dc2a7b92be28e8ed
brootle <luganskukraine@gmail.com>
2017-10-04 01:38:31 +0300

    add thumbnail table added

--- qencode5-portal/js/q5.datatable.js
+++ qencode5-portal/js/q5.datatable.js
@@ -35,10 +35,10 @@ $(document).ready(function(){
             { "width": "12%" },
             { "width": "10%" },
             null,
-            { "width": "10%" },
-            { "width": "35%" },
+            { "width": "25%" },
+            { "width": "20%" },
             { "bSortable": false, "width": "10%" },
-            { "bSortable": false }
+            { "bSortable": false, "width": "7%" }
         ],
 
         "dom": 'rt<"q5-table-navigation"ip><"clear">'
@@ -58,6 +58,62 @@ $(document).ready(function(){
 
 
     });
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+
+    var thumbnailTable = $('#thumbnail_table').DataTable( {  
+    
+        // see https://datatables.net/reference/option/language.aria.paginate.next
+        language: {
+            paginate: {
+                previous: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+                next:     '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+            },
+            aria: {
+                paginate: {
+                    previous: 'Previous',
+                    next:     'Next'
+                }
+            },
+            info: `<span class="q5-text-gray"> <span class="q5-table-visible-records">_START_ - _END_</span> of _TOTAL_</span>`
+        },
+
+        "iDisplayLength": 5,
+        "pagingType": "simple",     // see https://datatables.net/examples/basic_init/alt_pagination.html
+
+        rowReorder: {
+            selector: 'td:last-child'
+        },
+
+        "aoColumns": [
+            null,
+            { "bSortable": false, "width": "8%" },
+            { "width": "12%" },
+            { "width": "10%" },
+            null,
+            { "bSortable": false, "width": "10%" },
+            { "bSortable": false,  "width": "7%" }
+        ],
+
+        "dom": 'rt<"q5-table-navigation"ip><"clear">'
+        //"dom": '<"top"ip>rt<"bottom"ip><"clear">'
+
+    });
+
+
+    // event listener on row reorder
+    thumbnailTable.on( 'row-reorder', function ( e, diff, edit ) {
+
+        // info on row that was moved and triggered event
+        console.log(edit.triggerRow.data());
+        
+        // info on rows that changed their positions
+        console.log(diff);
+
+
+    });    
 
 
 });
