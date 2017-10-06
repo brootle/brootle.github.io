b4f8bf57e2d7987b4f058e9a8ee2c4bffb1bb3d5
brootle <luganskukraine@gmail.com>
2017-10-06 13:45:06 +0300

    adding last ttab for encoding profile page

--- qencode5-portal/js/q5.datatable.js
+++ qencode5-portal/js/q5.datatable.js
@@ -170,4 +170,47 @@ $(document).ready(function(){
     });        
 
 
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////    
+
+    var transferMethodTable = $('#transfer_method_table').DataTable( {  
+    
+        // see https://datatables.net/reference/option/language.aria.paginate.next
+        language: {
+            paginate: {
+                previous: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+                next:     '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+            },
+            aria: {
+                paginate: {
+                    previous: 'Previous',
+                    next:     'Next'
+                }
+            },
+            info: `<span class="q5-text-gray"> <span class="q5-table-visible-records">_START_ - _END_</span> of _TOTAL_</span>`
+        },
+
+        "iDisplayLength": 5,
+        "pagingType": "simple",     // see https://datatables.net/examples/basic_init/alt_pagination.html
+
+        rowReorder: false,
+
+        "aoColumns": [
+            null,
+            { "width": "45%" },
+            { "width": "45%" },
+            { "bSortable": false, "width": "10%" }
+        ],
+
+        "dom": 'rt<"q5-table-navigation"ip><"clear">'
+    });   
+
+    // find all "q5-tabcontent-with-tables" and remove this style
+    // basically we hide inactive content after tables were initiated
+    // we need to initate tables when they are displayed in order to set widths of columns
+    const tabContentWithTables = document.querySelectorAll('.q5-tabcontent-with-tables');
+    tabContentWithTables.forEach(tabContent => {
+        tabContent.classList.remove("q5-tabcontent-with-tables");
+    });          
+
 });
