5d3c7b878b4cf118afdd75daa2ac51653603a863
brootle <luganskukraine@gmail.com>
2018-03-25 17:30:38 +0300

    animation to header added

--- urunit/js/animation.js
+++ urunit/js/animation.js
@@ -1,5 +1,31 @@
 $(function () {
   
-    $(".slide-in").addClass("active");
-  
+    // run on page load
+    $("#header_logo").addClass("active");
+
+    // set delay for other animation not related to scroll
+    setTimeout(function() {
+        $(".header-title").addClass("active");
+    }, 400);    
+
+    setTimeout(function() {
+        $(".header-description").addClass("active");
+    }, 800);   
+
+    setTimeout(function() {
+        $("#btn_01").addClass("active");
+    }, 1200);      
+
+    setTimeout(function() {
+        $("#btn_02").addClass("active");
+    }, 1400);      
+
+    setTimeout(function() {
+        $("#btn_03").addClass("active");
+    }, 1600);     
+
+    setTimeout(function() {
+        $(".header-social-buttons").addClass("active");
+    }, 2000);         
+
 });
