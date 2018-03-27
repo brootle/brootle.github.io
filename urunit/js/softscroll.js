ec0ab2808cde3ea85c19e95cb95ae0411487cd0d
brootle <luganskukraine@gmail.com>
2018-03-27 22:27:40 +0300

    urunit deleted

--- urunit/js/softscroll.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  
-    $("a[href^='#']").click(function(e) { 
-      e.preventDefault(); 
-      var dest = $(this).attr('href'); 
-      $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow'); 
-    }); 
-  
-});
