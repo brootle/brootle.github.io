48d26a862c39267d3cfb6d84ff33117bc35445dc
brootle <luganskukraine@gmail.com>
2019-02-08 16:47:13 +0200

    menu js updated

--- qencode5/js/menu.js
+++ qencode5/js/menu.js
@@ -49,8 +49,25 @@ $(function () {
     // this will open and close submenu in mobile mode
     $('.q6-menu-item--has-submenu').click(function(e) {
         e.preventDefault();
-        $(this).toggleClass('q6-menu-item--submenu-closed');     
-    });
+
+        var currentSubmenu = this;
+
+        // 1. find open submenues (not current) and hide it
+        $('.q6-menu-item--has-submenu').each(function(index){
+            if(this !== currentSubmenu){
+                $(this).addClass("q6-menu-item--submenu-closed");
+            }
+        });   
+        
+        // 2. show or hide current submenu on click
+        if($(this).hasClass("q6-menu-item--submenu-closed")){
+            $(this).removeClass("q6-menu-item--submenu-closed");    
+        } else{
+            $(this).addClass("q6-menu-item--submenu-closed");  
+        }
+
+    });    
+
 
     // close menu in mobile mode after we click the menu item in submenu
     $(".q6-submenu-box__block").click(function(e) {
@@ -58,8 +75,19 @@ $(function () {
         if(mobileMode){
             $(".hamburger").removeClass("is-active");
             $(".q6-top-menu--mobile-mode").addClass("q6-hidden"); 
-        }         
+        }                 
     });
 
-});           
+    // close mobile menu when we click menu link
+    // and close open submenues
+    $(".q6-menu-item-link").click(function(e) {
+        var mobileMode = $('.hamburger').is(':visible');
+        if(mobileMode){
+            $(".hamburger").removeClass("is-active");
+            $(".q6-top-menu--mobile-mode").addClass("q6-hidden"); 
+        }      
+        
+        $('.q6-menu-item--has-submenu').addClass("q6-menu-item--submenu-closed");  
+    });    
 
+});           
